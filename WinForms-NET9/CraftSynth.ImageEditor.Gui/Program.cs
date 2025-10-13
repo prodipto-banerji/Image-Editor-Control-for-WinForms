42c6bb98b829a658efd510d5da7bd7c5f8f3cde0
prodipto-banerji <prodipto_banerji@epam.com>
2025-10-14 00:34:58 +0530

    Add NET9 Program.cs with ApplicationConfiguration.Initialize

---
+++ WinForms-NET9/CraftSynth.ImageEditor.Gui/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+namespace CraftSynth.ImageEditor.Gui
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
