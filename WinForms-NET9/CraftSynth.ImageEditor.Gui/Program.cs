1e16916cd4d32707f5fa27ce43439db3e062bbfb
prodipto-banerji <prodipto_banerji@epam.com>
2025-10-14 00:45:26 +0530

    Use classic WinForms initialization in Program.cs for NET9

--- WinForms-NET9/CraftSynth.ImageEditor.Gui/Program.cs
+++ WinForms-NET9/CraftSynth.ImageEditor.Gui/Program.cs
@@ -8,7 +8,8 @@ namespace CraftSynth.ImageEditor.Gui
         [STAThread]
         private static void Main()
         {
-            ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
