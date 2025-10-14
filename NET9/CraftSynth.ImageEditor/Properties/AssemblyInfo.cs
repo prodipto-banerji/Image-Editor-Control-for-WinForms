465795da2e7086e72646a3f40aef75705aafb74f
prodipto-banerji <prodipto_banerji@epam.com>
2025-10-15 00:38:40 +0530

    Migrate AssemblyInfo.cs to .NET 9 - simplified assembly attributes for modern SDK-style project

---
+++ NET9/CraftSynth.ImageEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CraftSynth.ImageEditor")]
+[assembly: AssemblyDescription("CraftSynth.ImageEditor with several enhancements - migrated to .NET 9 with SkiaSharp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MSDC")]
+[assembly: AssemblyProduct("CraftSynth.ImageEditor")]
+[assembly: AssemblyCopyright("Copyright © 2015-2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components. If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8635dc7b-dc93-4b01-8c68-6c4a1a61ea50")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+
+// .NET 9 specific attributes for cross-platform compatibility
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("linux")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("macos")]
