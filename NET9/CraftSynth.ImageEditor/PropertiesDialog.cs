cc283221d5ba9545e29cce0dedd1da29c0e0da61
prodipto-banerji <prodipto_banerji@epam.com>
2025-10-15 00:47:18 +0530

    Migrate PropertiesDialog.cs from .NET 4.8 to .NET 9 with SkiaSharp integration

---
+++ NET9/CraftSynth.ImageEditor/PropertiesDialog.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+using SkiaSharp;
+
+namespace CraftSynth.ImageEditor
+{
+    internal partial class PropertiesDialog : Form
+    {
+        public PropertiesDialog()
+        {
+            InitializeComponent();
+        }
+
+        private GraphicsProperties properties;
+        private const string undefined = "??";
+        private const int maxWidth = 5;
+
+        public GraphicsProperties Properties
+        {
+            get { return properties; }
+            set { properties = value; }
+        }
+
+        private void PropertiesDialog_Load(object sender, EventArgs e)
+        {
+            InitControls();
+            SetColor();
+            SetPenWidth();
+        }
+
+        private void InitControls()
+        {
+            for (int i = 1; i <= maxWidth; i++)
+            {
+                cmbPenWidth.Items.Add(i.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+
+        private void SetColor()
+        {
+            if (properties.Color.HasValue)
+            {
+                // Convert SKColor to System.Drawing.Color for WinForms compatibility
+                var skColor = properties.Color.Value;
+                lblColor.BackColor = System.Drawing.Color.FromArgb(
+                    skColor.Alpha, 
+                    skColor.Red, 
+                    skColor.Green, 
+                    skColor.Blue);
+            }
+            else
+            {
+                lblColor.Text = undefined;
+            }
+        }
+
+        private void SetPenWidth()
+        {
+            if (properties.PenWidth.HasValue)
+            {
+                int penWidth = properties.PenWidth.Value;
+
+                if (penWidth < 1)
+                    penWidth = 1;
+
+                if (penWidth > maxWidth)
+                    penWidth = maxWidth;
+
+                label2.Text = penWidth.ToString(CultureInfo.InvariantCulture);
+                cmbPenWidth.SelectedIndex = penWidth - 1;
+            }
+            else
+            {
+                label2.Text = undefined;
+            }
+        }
+
+        private void ReadValues()
+        {
+            if (cmbPenWidth.Text != undefined)
+            {
+                properties.PenWidth = cmbPenWidth.SelectedIndex + 1;
+            }
+
+            if (lblColor.Text.Length == 0)
+            {
+                // Convert System.Drawing.Color to SKColor
+                var drawingColor = lblColor.BackColor;
+                properties.Color = new SKColor(
+                    drawingColor.R,
+                    drawingColor.G,
+                    drawingColor.B,
+                    drawingColor.A);
+            }
+        }
+
+        private void cmbPenWidth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int width = cmbPenWidth.SelectedIndex + 1;
+            lblPenWidth.Text = width.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void btnSelectColor_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog dlg = new ColorDialog())
+            {
+                dlg.Color = lblColor.BackColor;
+
+                if (dlg.ShowDialog(this) == DialogResult.OK)
+                {
+                    lblColor.BackColor = dlg.Color;
+                    lblColor.Text = "";
+                }
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            ReadValues();
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
