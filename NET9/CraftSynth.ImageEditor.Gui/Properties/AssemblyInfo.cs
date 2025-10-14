23a4eecd3b664a4012d4bbe9910aacacd79063d7
prodipto-banerji <prodipto_banerji@epam.com>
2025-10-15 01:27:12 +0530

    Migrate CraftSynth.ImageEditor.Gui AssemblyInfo.cs to .NET 9

---
+++ NET9/CraftSynth.ImageEditor.Gui/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3cd732ec-b1ba-46aa-ba2f-7b5b76235d86")]
+
+// Additional assembly metadata can be specified here if needed
+// Note: In .NET 9, most assembly attributes are now generated automatically
+// from the project file properties. The following attributes are typically
+// handled by MSBuild properties in the .csproj file:
+//   - AssemblyTitle -> <AssemblyTitle>
+//   - AssemblyDescription -> <Description>
+//   - AssemblyConfiguration -> <Configuration>  
+//   - AssemblyCompany -> <Company>
+//   - AssemblyProduct -> <Product>
+//   - AssemblyCopyright -> <Copyright>
+//   - AssemblyTrademark -> <AssemblyTrademark>
+//   - AssemblyCulture -> <AssemblyCulture>
+//   - AssemblyVersion -> <AssemblyVersion>
+//   - AssemblyFileVersion -> <FileVersion>
